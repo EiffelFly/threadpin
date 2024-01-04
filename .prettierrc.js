7f5c8fb9d2a64d834292197bc76c6e56f3f5ed36
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-01-04 14:00:01 +0800

    chore: add tailwind prettier plugin

---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 80,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+  overrides: [
+    {
+      files: [".*", "*.json", "*.md", "*.toml", "*.yml"],
+      options: {
+        useTabs: false,
+      },
+    },
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
