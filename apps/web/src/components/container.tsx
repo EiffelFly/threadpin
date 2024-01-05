032df8ae48a737b30543fd5b468b0ce1469c1f04
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-01-05 21:58:01 +0800

    feat: update the container to correctly handle pasted URL

--- apps/web/src/components/container.tsx
+++ apps/web/src/components/container.tsx
@@ -2,23 +2,15 @@
 
 import * as React from "react";
 import { cn } from "@/lib/utils";
+import { toast } from "sonner";
+import { ProcessingURLDrawer } from "./processing-url-drawer";
+import { Nullable } from "@/types";
 
 export const Container = ({ children }: { children: React.ReactNode }) => {
   const [isOnEdge, setIsOnEdge] = React.useState(false);
-  const [showRotateGradient, setShowRotateGradient] = React.useState(false);
-
-  // React.useEffect(() => {
-  //   let timer: NodeJS.Timeout;
-
-  //   if (!isOnEdge) {
-  //     setShowRotateGradient(false);
-  //     return;
-  //   }
-
-  //   timer = setTimeout(() => {
-  //     setShowRotateGradient(true);
-  //   }, 1000);
-  // }, [isOnEdge]);
+  const [openProcessingURLDrawer, setOpenProcessingURLDrawer] =
+    React.useState(false);
+  const [url, setUrl] = React.useState<Nullable<URL>>(null);
 
   return (
     <div
@@ -29,6 +21,22 @@ export const Container = ({ children }: { children: React.ReactNode }) => {
       onMouseOut={(e) => {
         e.stopPropagation();
         setIsOnEdge(false);
+      }}
+      onPaste={(e) => {
+        const pastedString = e.clipboardData.getData("text/plain");
+        if (pastedString) {
+          try {
+            const url = new URL(pastedString);
+            setOpenProcessingURLDrawer(true);
+            console.log("url", url);
+            setUrl(url);
+          } catch (err) {
+            toast.error("Invalid URL", {
+              duration: 1000,
+            });
+            setUrl(null);
+          }
+        }
       }}
       className="relative w-full p-[var(--root-padding)]"
     >
@@ -41,6 +49,9 @@ export const Container = ({ children }: { children: React.ReactNode }) => {
           "border-border rotate-gradient bg-background z-10 flex h-[calc(100vh-calc(var(--root-padding)*2))] w-[calc(100vw-calc(var(--root-padding)*2))] flex-row rounded-lg border p-[1px] transition-colors duration-500",
           isOnEdge ? "hover" : ""
         )}
+        onPaste={(e) => {
+          e.preventDefault();
+        }}
       >
         {children}
       </div>
