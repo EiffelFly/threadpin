3b74748a1a2528cc17174d91ddb02423fafdd87e
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-01-07 22:24:03 +0800

    feat: add login UI

---
+++ apps/web/src/app/login/AuthUI.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useSupabase } from "../supabase-provider";
+import { getURL } from "@/lib/utils";
+
+export default function AuthUI() {
+  const { supabase } = useSupabase();
+  return (
+    <div className="flex flex-col space-y-4">
+      <Auth
+        supabaseClient={supabase}
+        providers={[]}
+        redirectTo={`${getURL()}/auth/callback`}
+        magicLink={false}
+        appearance={{
+          theme: ThemeSupa,
+          variables: {
+            default: {
+              colors: {
+                brand: "#404040",
+                brandAccent: "#52525b",
+              },
+            },
+          },
+        }}
+        theme="dark"
+      />
+    </div>
+  );
+}
