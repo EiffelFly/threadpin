50542408e820e2f4e609e245260fb38f78a41af4
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-01-07 14:18:20 +0800

    feat: imporve ProcessingURLDrawer component

--- apps/web/src/components/processing-url-drawer/processing-url-drawer.tsx
+++ apps/web/src/components/processing-url-drawer/processing-url-drawer.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import * as React from "react";
-import { Drawer, DrawerContent } from "./ui/drawer";
-import { getURLDomainIcon } from "@/lib/url";
+import { Drawer, DrawerContent } from "../ui/drawer";
 import { Store } from "@/store/type";
 import { usePinStore } from "@/store/usePinStore";
 import { useShallow } from "zustand/react/shallow";
 import axios from "axios";
+import { Nullable } from "@/types";
+import { GetURLDataResponse } from "@/app/api/scraping/route";
+import { cn } from "@/lib/utils";
+import { URLMetadata } from "./url-metadata";
+import { URLDomainIcon } from "../ui/url-domain-icon";
 
 const selector = (store: Store) => ({
   processingURLDrawerState: store.processingURLDrawerState,
@@ -20,6 +24,8 @@ export const ProcessingURLDrawer = () => {
   } = usePinStore(useShallow(selector));
 
   const [processingURL, setProcessingURL] = React.useState(false);
+  const [urlData, setURLData] =
+    React.useState<Nullable<GetURLDataResponse>>(null);
 
   React.useEffect(() => {
     async function getURLData() {
@@ -27,7 +33,10 @@ export const ProcessingURLDrawer = () => {
 
       try {
         setProcessingURL(true);
-        const data = await axios.get("/api/scraping?url=" + url.href);
+        const { data } = await axios.get<GetURLDataResponse>(
+          "/api/scraping?url=" + url.href
+        );
+        setURLData(data);
         setProcessingURL(false);
       } catch (error) {}
     }
@@ -43,13 +52,25 @@ export const ProcessingURLDrawer = () => {
           ...prev,
           open,
         }));
+        setURLData(null);
+        setProcessingURL(false);
       }}
     >
       <DrawerContent>
         {url ? (
-          <div className="mx-auto flex min-h-[360px] w-full max-w-2xl flex-col">
-            <div className="mx-auto flex h-6 w-6 rounded-lg">
-              {getURLDomainIcon(url, "w-4 h-4 m-auto")}
+          <div className="mx-auto flex min-h-[600px] w-full max-w-[800px] flex-row p-10">
+            <div
+              className={cn(
+                "mx-auto flex w-[400px] flex-col transition-transform duration-700 ease-in-out",
+                urlData ? "-translate-x-1/2" : ""
+              )}
+            >
+              <URLDomainIcon url={url} className="stroke-background h-4 w-4" />
+              {urlData ? (
+                <URLMetadata url={url} data={urlData} />
+              ) : (
+                <URLMetadata.Skeleton />
+              )}
             </div>
           </div>
         ) : null}
---
+++ apps/web/src/components/processing-url-drawer/url-metadata.tsx
@@ -0,0 +1,44 @@
+import { GetURLDataResponse } from "@/app/api/scraping/route";
+import { ImageWithFallback } from "../ui/image-with-fallback";
+import { Skeleton } from "../ui/skeleton";
+
+const URLMetadataSkeleton = () => {
+  return (
+    <div className="border-border flex w-full flex-col rounded-md border">
+      <Skeleton className="h-[209px] w-full rounded-b-none" />
+      <div className="flex flex-col gap-y-3 p-4">
+        <Skeleton className="h-10 w-full" />
+        <Skeleton className="h-5 w-2/3" />
+        <Skeleton className="h-3 w-2/3" />
+      </div>
+    </div>
+  );
+};
+
+export const URLMetadata = ({
+  url,
+  data,
+}: {
+  url: URL;
+  data: GetURLDataResponse;
+}) => {
+  return (
+    <div className="border-border flex w-full flex-col rounded-md border">
+      <div className="relative h-[209px] w-full">
+        <ImageWithFallback
+          src={data.ogImage}
+          alt={data.title ?? null}
+          fallbackImg={<div className="bg-muted h-[209px] w-full" />}
+        />
+      </div>
+      <div className="flex flex-col p-4">
+        <h3 className="typography-h3">{data.title}</h3>
+        {data.description ? (
+          <p className="typography-p mb-0.5">{data.description}</p>
+        ) : null}
+        <p className="typography-muted">{url.href}</p>
+      </div>
+    </div>
+  );
+};
+URLMetadata.Skeleton = URLMetadataSkeleton;
