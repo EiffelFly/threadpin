a17f2a437e2df4398e8506aa438d6491ecdf7744
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-01-12 14:01:10 +0800

    chore: reinit the db

---
+++ apps/web/supabase/migrations/20240112052512_foundation.sql
@@ -0,0 +1,201 @@
+create table "public"."items" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "name" text,
+    "og_image" text,
+    "url" text,
+    "description" text
+);
+
+
+alter table "public"."items" enable row level security;
+
+create table "public"."lists" (
+    "uid" uuid not null default gen_random_uuid(),
+    "created_at" timestamp with time zone not null default now(),
+    "name" text,
+    "items" text[],
+    "id" text
+);
+
+
+alter table "public"."lists" enable row level security;
+
+create table "public"."profiles" (
+    "id" uuid not null,
+    "created_at" timestamp with time zone not null default now(),
+    "first_name" text,
+    "last_name" text,
+    "avatar_url" text
+);
+
+
+alter table "public"."profiles" enable row level security;
+
+CREATE UNIQUE INDEX item_pkey ON public.items USING btree (id);
+
+CREATE UNIQUE INDEX list_pkey ON public.lists USING btree (uid);
+
+CREATE UNIQUE INDEX profiles_pkey ON public.profiles USING btree (id);
+
+alter table "public"."items" add constraint "item_pkey" PRIMARY KEY using index "item_pkey";
+
+alter table "public"."lists" add constraint "list_pkey" PRIMARY KEY using index "list_pkey";
+
+alter table "public"."profiles" add constraint "profiles_pkey" PRIMARY KEY using index "profiles_pkey";
+
+alter table "public"."profiles" add constraint "profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
+
+alter table "public"."profiles" validate constraint "profiles_id_fkey";
+
+grant delete on table "public"."items" to "anon";
+
+grant insert on table "public"."items" to "anon";
+
+grant references on table "public"."items" to "anon";
+
+grant select on table "public"."items" to "anon";
+
+grant trigger on table "public"."items" to "anon";
+
+grant truncate on table "public"."items" to "anon";
+
+grant update on table "public"."items" to "anon";
+
+grant delete on table "public"."items" to "authenticated";
+
+grant insert on table "public"."items" to "authenticated";
+
+grant references on table "public"."items" to "authenticated";
+
+grant select on table "public"."items" to "authenticated";
+
+grant trigger on table "public"."items" to "authenticated";
+
+grant truncate on table "public"."items" to "authenticated";
+
+grant update on table "public"."items" to "authenticated";
+
+grant delete on table "public"."items" to "service_role";
+
+grant insert on table "public"."items" to "service_role";
+
+grant references on table "public"."items" to "service_role";
+
+grant select on table "public"."items" to "service_role";
+
+grant trigger on table "public"."items" to "service_role";
+
+grant truncate on table "public"."items" to "service_role";
+
+grant update on table "public"."items" to "service_role";
+
+grant delete on table "public"."lists" to "anon";
+
+grant insert on table "public"."lists" to "anon";
+
+grant references on table "public"."lists" to "anon";
+
+grant select on table "public"."lists" to "anon";
+
+grant trigger on table "public"."lists" to "anon";
+
+grant truncate on table "public"."lists" to "anon";
+
+grant update on table "public"."lists" to "anon";
+
+grant delete on table "public"."lists" to "authenticated";
+
+grant insert on table "public"."lists" to "authenticated";
+
+grant references on table "public"."lists" to "authenticated";
+
+grant select on table "public"."lists" to "authenticated";
+
+grant trigger on table "public"."lists" to "authenticated";
+
+grant truncate on table "public"."lists" to "authenticated";
+
+grant update on table "public"."lists" to "authenticated";
+
+grant delete on table "public"."lists" to "service_role";
+
+grant insert on table "public"."lists" to "service_role";
+
+grant references on table "public"."lists" to "service_role";
+
+grant select on table "public"."lists" to "service_role";
+
+grant trigger on table "public"."lists" to "service_role";
+
+grant truncate on table "public"."lists" to "service_role";
+
+grant update on table "public"."lists" to "service_role";
+
+grant delete on table "public"."profiles" to "anon";
+
+grant insert on table "public"."profiles" to "anon";
+
+grant references on table "public"."profiles" to "anon";
+
+grant select on table "public"."profiles" to "anon";
+
+grant trigger on table "public"."profiles" to "anon";
+
+grant truncate on table "public"."profiles" to "anon";
+
+grant update on table "public"."profiles" to "anon";
+
+grant delete on table "public"."profiles" to "authenticated";
+
+grant insert on table "public"."profiles" to "authenticated";
+
+grant references on table "public"."profiles" to "authenticated";
+
+grant select on table "public"."profiles" to "authenticated";
+
+grant trigger on table "public"."profiles" to "authenticated";
+
+grant truncate on table "public"."profiles" to "authenticated";
+
+grant update on table "public"."profiles" to "authenticated";
+
+grant delete on table "public"."profiles" to "service_role";
+
+grant insert on table "public"."profiles" to "service_role";
+
+grant references on table "public"."profiles" to "service_role";
+
+grant select on table "public"."profiles" to "service_role";
+
+grant trigger on table "public"."profiles" to "service_role";
+
+grant truncate on table "public"."profiles" to "service_role";
+
+grant update on table "public"."profiles" to "service_role";
+
+create policy "Public profiles are viewable by everyone."
+on "public"."profiles"
+as permissive
+for select
+to public
+using (true);
+
+
+create policy "Users can insert their own profile."
+on "public"."profiles"
+as permissive
+for insert
+to authenticated
+with check ((auth.uid() = id));
+
+
+create policy "Users can update own profile."
+on "public"."profiles"
+as permissive
+for update
+to authenticated
+using ((auth.uid() = id));
+
+
+
