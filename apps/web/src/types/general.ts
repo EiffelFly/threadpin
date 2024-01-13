da7df367d9048ad6b79a4f5c719c7105dc67c2c5
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-01-13 23:48:59 +0800

    feat: update types

--- apps/web/src/types/general.ts
+++ apps/web/src/types/general.ts
@@ -1,3 +1,10 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+import { Database } from "./database.types";
+
 export type Nullable<T> = T | null;
 
 export type GeneralRecord = Record<string, any>;
+
+export type ItemsDisplayMode = "list" | "rich-list";
+
+export type TypedSupabaseClient = SupabaseClient<Database>;
