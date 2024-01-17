6559bd2126d4e8c2cc22bcf723b04a282628672d
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-01-17 20:52:07 +0800

    feat: update specific client type

--- apps/web/src/types/general.ts
+++ apps/web/src/types/general.ts
@@ -8,3 +8,15 @@ export type GeneralRecord = Record<string, any>;
 export type ItemsDisplayMode = "list" | "rich-list";
 
 export type TypedSupabaseClient = SupabaseClient<Database>;
+
+export type SelectOption = {
+  label: string;
+  value: string;
+  startIcon?: React.ReactNode;
+  endIcon?: React.ReactNode;
+};
+
+export type ClientUserListItem = Pick<
+  Database["public"]["Tables"]["list_items"]["Row"],
+  "uid" | "user_id"
+> & { items: Nullable<Database["public"]["Tables"]["items"]["Row"]> };
