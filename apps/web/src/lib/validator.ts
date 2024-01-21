574a0f337af669edad386937b960c808eef9cd5c
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-01-21 22:52:19 +0800

    feat: add ZodImageValidator

--- apps/web/src/lib/validator.ts
+++ apps/web/src/lib/validator.ts
@@ -6,3 +6,21 @@ export const ListsOrderRecordSchema = z.array(
     order: z.number(),
   })
 );
+
+export const ACCEPTED_IMAGE_TYPES = [
+  "image/jpeg",
+  "image/jpg",
+  "image/png",
+  "image/webp",
+];
+
+const MAX_FILE_SIZE = 500000;
+
+export const ZodImageValidator = z
+  .any()
+  .refine((files) => files?.length == 1, "Image is required.")
+  .refine((files) => files?.[0]?.size <= MAX_FILE_SIZE, `Max file size is 5MB.`)
+  .refine(
+    (files) => ACCEPTED_IMAGE_TYPES.includes(files?.[0]?.type),
+    ".jpg, .jpeg, .png and .webp files are accepted."
+  );
