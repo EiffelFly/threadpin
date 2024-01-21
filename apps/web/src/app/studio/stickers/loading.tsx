968c6fba45b35a04c41662cc8c86ad534243e233
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-01-21 22:51:51 +0800

    feat: add stickers related page

---
+++ apps/web/src/app/studio/stickers/loading.tsx
@@ -0,0 +1,5 @@
+import { StickerLoading } from "@/components/stickers/sticker-loading";
+
+export default function StickersLoadingPage() {
+  return <StickerLoading />;
+}
---
+++ apps/web/src/app/studio/stickers/page.tsx
@@ -0,0 +1,38 @@
+import { Stickers } from "@/components/stickers/stickers";
+import { prefetchUserStickers } from "@/react-query/queries/use-user-stickers";
+import { getUserQuery } from "@/supabase-query";
+import { Database } from "@/types/database.types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function StickersPage() {
+  const supabaseClient = createServerComponentClient<Database>({
+    cookies,
+  });
+
+  const { data } = await getUserQuery(supabaseClient);
+
+  if (!data.user) {
+    redirect("/login");
+  }
+
+  const queryClient = new QueryClient();
+
+  await prefetchUserStickers({
+    user_id: data.user.id,
+    queryClient,
+    supabaseClient,
+  });
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Stickers />
+    </HydrationBoundary>
+  );
+}
