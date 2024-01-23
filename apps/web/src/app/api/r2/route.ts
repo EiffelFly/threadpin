17c528109737fe37f0df88dbd2694d3a20b7c38f
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-01-23 10:29:48 +0800

    feat: add /api/r2 delete handler

--- apps/web/src/app/api/r2/route.ts
+++ apps/web/src/app/api/r2/route.ts
@@ -1,4 +1,5 @@
 import { createR2Object } from "@/aws-query/create-r2-object";
+import { deleteR2Object } from "@/aws-query/delete-r2-object";
 import { S3Client } from "@aws-sdk/client-s3";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -14,11 +15,13 @@ const r2Client = new S3Client({
 export async function POST(request: NextRequest) {
   let file: File;
   let key: string;
+  let type: string;
 
   try {
     const formData = await request.formData();
     file = formData.get("file") as File;
     key = formData.get("key") as string;
+    type = formData.get("type") as string;
   } catch (error) {
     console.log(error);
     return NextResponse.json({ error });
@@ -32,13 +35,39 @@ export async function POST(request: NextRequest) {
     return NextResponse.json({ error: "Key is required." }, { status: 400 });
   }
 
+  if (!type) {
+    return NextResponse.json({ error: "Type is required." }, { status: 400 });
+  }
+
   const buffer = Buffer.from(await (file as File).arrayBuffer());
 
   try {
     await createR2Object({
       client: r2Client,
       file: buffer,
-      key: key as string,
+      key: key,
+      type,
+    });
+
+    const asset_url = `${process.env.R2_PUBLIC_ENDPOINT}/${key}`;
+
+    return NextResponse.json({ success: true, data: { asset_url } });
+  } catch (error) {
+    return NextResponse.json({ error });
+  }
+}
+
+export async function Delete(request: NextRequest) {
+  const res = await request.json();
+
+  if (!res.key) {
+    return NextResponse.json({ error: "Key is required." }, { status: 400 });
+  }
+
+  try {
+    await deleteR2Object({
+      client: r2Client,
+      key: res.key,
     });
 
     return NextResponse.json({ success: true });
