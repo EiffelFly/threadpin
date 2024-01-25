3c3fabea149fde389207f37bfb644190c0fa70ff
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2024-01-25 11:41:19 +0800

    feat: add more database helper types

--- apps/web/src/types/general.ts
+++ apps/web/src/types/general.ts
@@ -20,3 +20,19 @@ export type ClientUserListItem = Pick<
   Database["public"]["Tables"]["list_items"]["Row"],
   "uid" | "user_id"
 > & { items: Nullable<Database["public"]["Tables"]["items"]["Row"]> };
+
+export type NodeType = "sticker_node";
+
+export type StickerNodeData = {
+  sticker_uid: Nullable<string>;
+  sticker: Nullable<Sticker>;
+  list_uid: Nullable<string>;
+  list: Nullable<List>;
+};
+
+export type NodeData = StickerNodeData;
+
+export type Sticker = Database["public"]["Tables"]["stickers"]["Row"];
+export type List = Database["public"]["Tables"]["lists"]["Row"];
+export type ListItem = Database["public"]["Tables"]["list_items"]["Row"];
+export type Item = Database["public"]["Tables"]["items"]["Row"];
